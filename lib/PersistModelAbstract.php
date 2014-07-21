fffef7366b52ddbfbe44a04f4708336656443fd0
Sérgio Novelli <sernovelli@users.noreply.github.com>
2014-07-21 16:46:05 -0300

    Alteração de conexão
    Alterando a conexão com BD.

--- lib/PersistModelAbstract.php
+++ lib/PersistModelAbstract.php
@@ -15,18 +15,8 @@ abstract class PersistModelAbstract
 	
 	function __construct() {
 		try {
-				
-			// servidor remoto - Produção
-			 $this->o_db = new PDO('mysql:host=localhost;dbname=dinamica_geraarquivos', 'dinamica_garquiv', '2q5r4dcplm', array(PDO::ATTR_PERSISTENT => true)) or print (mysql_error());
-			
-			// servidor remoto - Homologação
-			// $this->o_db = new PDO('mysql:host=localhost;dbname=dinamirh_geraarquivos', 'dinamirh_garquiv', '2q5r4dcplm', array(PDO::ATTR_PERSISTENT => true)) or print (mysql_error());
-			
-			// servidor local - Windows 2008 - Testes
-			//$this->o_db = new PDO('mysql:host=192.168.10.249;port=3306;dbname=dinamirh_geraarquivos', 'teste', 'teste', array(PDO::ATTR_PERSISTENT => true)) or die(mysql_error());
-			
 			// localhost - Desenvolvimento
-			//$this->o_db = new PDO('mysql:host=localhost;dbname=geraarquivos', 'root', '', array(PDO::ATTR_PERSISTENT => true)) or print (mysql_error());
+			$this->o_db = new PDO('mysql:host=localhost;dbname=nome_do_banco', 'root', '', array(PDO::ATTR_PERSISTENT => true)) or print (mysql_error());
 			
 			$this->o_db->setAttribute ( PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION );
 		} catch (PDOException  $e) {
