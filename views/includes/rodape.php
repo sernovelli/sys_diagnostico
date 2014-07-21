3e0b5e44347cf726e8401011cbcccf4c241079f7
Sérgio Novelli <sernovelli@users.noreply.github.com>
2014-07-21 16:56:16 -0300

    Alterando o rodapé
    Alterado o rodapé do sistema para tornar o projeto livre.

--- views/includes/rodape.php
+++ views/includes/rodape.php
@@ -1,6 +1,3 @@
 <footer id="rodape">
-	Geraarquivos v1.0 - Todos os direitos reservados. &copy; 2013 | 
-	<a href="http://www.alkantara.com.br" target="_blank">
-		<img src="images/alkantara.png" />
-	</a>
+	Diagnostic -  Projeto livre
 </footer>
